4441a006ba658b96b5f93e236c05a59c9ed1c92f
adharsh-yesugari <113429862+adharsh-yesugari@users.noreply.github.com>
2024-09-09 09:57:21 +0530

    Update AdminService.java
    Added getFaculty Interface

--- main/com/invigilationduty/admin/service/AdminService.java
+++ main/com/invigilationduty/admin/service/AdminService.java
@@ -7,13 +7,15 @@ import main.com.invigilationduty.faculty.entity.Faculty;
 import java.util.List;
 
 public interface AdminService {
-    public boolean registerAdmin(Admin admin);
+    boolean registerAdmin(Admin admin);
 
-    public Admin loginAdmin(Admin admin);
+    Admin loginAdmin(Admin admin);
 
-    public boolean addBlockFloorRoomNumber(Hall hall);
+    boolean addBlockFloorRoomNumber(Hall hall);
 
-    public List<Faculty> selectFacultyWithLeisureTime(String leisureTime);
+    List<Faculty> selectFacultyWithLeisureTime(String leisureTime);
 
     Faculty allocateRandomHalltoFacultySpecified(Faculty faculty);
+
+    List<Faculty> getFacultyList();
 }
